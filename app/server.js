e1614960d0aeec2dc3ca82a4bdd08c8d88b5fcc6
Scott Johnson <sjohnson.wvu@gmail.com>
2018-07-24 00:02:13 -0400

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// EXPRESS CONFIGURATION
+// =============================================================
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Set an initial port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTER
+// =============================================================
+// Route files to respond to user requests
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// =============================================================
+// Start the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
